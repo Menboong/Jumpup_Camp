cbc776ae85bf20c41f565a0bbc492bccd697275d
Menboong <ruby020415@naver.com>
2018-07-23 19:24:58 +0900

    button_control_2
    I made this code at jumpup_camp.
    When you push button, the led will turn on, when you push again, the led will turn off.

---
+++ 7.23/arduino/button_control/button_control_2/button_control_2.ino
@@ -0,0 +1,27 @@
+int Led = 13; // define LED Interface
+int buttonpin = 3; // define the key switch sensor interface
+int val; // define numeric variables val
+int flag=0;
+void setup() {
+pinMode(Led, OUTPUT); // define LED as output interface
+pinMode(buttonpin, INPUT); // define the key switch sensor output interface
+}
+
+void loop() {
+// When the key switch when the sensor detects a signal, LED flashes
+if (digitalRead(buttonpin) == LOW)
+{
+  delay(30);
+  if(flag == 0)
+  {
+    digitalWrite(Led,HIGH);
+    flag = 1;
+  }
+  else
+  {
+    digitalWrite(Led,LOW);
+    flag = 0;
+  }
+  delay(30);
+}
+}
