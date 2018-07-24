ea30b66836b4b891b6d6ef5f4c925510a28f08ae
Menboong <ruby020415@naver.com>
2018-07-24 23:27:50 +0900

    lazer
    I made this code at jumpup_camp.
    I add a function that button and sounds.

--- 7.24/lazer/lazer/lazer.ino
+++ 7.24/lazer/lazer/lazer.ino
@@ -1,3 +1,6 @@
+int flag = 0;
+int ck = 0;
+
 void setup() {
   Serial.begin(9600);
   pinMode(3,INPUT);//BUTTON
@@ -6,16 +9,38 @@ void setup() {
 }
 
 void loop() {
-  digitalWrite(9,1);
+  if(digitalRead(3) == 0)
+  {
+    delay(30);
+    if(flag == 0)
+    {
+      digitalWrite(9,1);
+      flag = 1;
+      ck = 1;
+    }
+    else
+    {
+     digitalWrite(9,0);
+     flag = 0;
+     ck = 0;
+    }
+   while(digitalRead(3)==0);
+   delay(30);
+  }
+while(ck == 1)
+{
   if(analogRead(A0)>=100)
   {
+    Serial.println(analogRead(A0));
     digitalWrite(4,1);
     delay(50);
     digitalWrite(4,0);
     delay(50);
-  }
-  else
-  {
+   }
+   else
+   {
     digitalWrite(4,0);
-  }
+   }
+   if(digitalRead(3)==0) break;
+}
 }
