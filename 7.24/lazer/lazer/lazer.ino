cbf96ab1d684e30c68d18cd76f13793bd6933cbe
Menboong <ruby020415@naver.com>
2018-07-24 23:25:05 +0900

    lazer_button
    I made this at jumpup_camp.
    I add a function that when you push button, the lazer will turn off. when you push again, lazer will turn on.

--- 7.24/lazer/lazer/lazer.ino
+++ 7.24/lazer/lazer/lazer.ino
@@ -1,16 +1,21 @@
 void setup() {
   Serial.begin(9600);
-  pinMode(3,INPUT);
-  pinMode(9,OUTPUT);
+  pinMode(3,INPUT);//BUTTON
+  pinMode(4,OUTPUT);//BUZZER
+  pinMode(9,OUTPUT);//LAZER
 }
 
 void loop() {
   digitalWrite(9,1);
-  Serial.println(analogRead(A0));
-  delay(2000);
-  //digitalWrite(9,0);
-  //Serial.println(analogRead(A0));
-  //delay(2000);
-  
-  
+  if(analogRead(A0)>=100)
+  {
+    digitalWrite(4,1);
+    delay(50);
+    digitalWrite(4,0);
+    delay(50);
+  }
+  else
+  {
+    digitalWrite(4,0);
+  }
 }
