5fb8ca8b448eb3e88616b4e835eb50bfdeb0a093
Menboong <ruby020415@naver.com>
2018-07-25 23:27:09 +0900

    favorite_stars
    I made this at jumpup_camp.
    This program can show profile of stars. You can see profile that you want.
    I use combobox,button,picturebox,label.

---
+++ 7.25/favorite_stars/favorite_stars/Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace favorite_stars
+{
+    public partial class favorite_star : Form
+    {
+        public favorite_star()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (comboBox1.SelectedItem.ToString() == "김재환")
+            {
+                pictureBox1.Load(@"D:\kjh.jpg");
+                pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+                label1.Text = "이름: 김재환 \n\n 생일:1996년 5월 27일 (22세) \n\n 키:175cm";
+            }
+            if (comboBox1.SelectedItem.ToString() == "백예린")
+            {
+                pictureBox1.Load(@"D:\byr.jpg");
+                pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+                label1.Text = "이름: 백예린 \n\n 생일:1997년 6월 26일 (21세) \n\n 키:165cm";
+            }
+            if (comboBox1.SelectedItem.ToString() == "아델")
+            {
+                pictureBox1.Load(@"D:\adele.jpg");
+                pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+                label1.Text = "이름: 아델 \n\n 본명: Adele Laurie Blue Adkins\n\n 생일:1988년 5월 5일 (30세) (22세)";
+            }
+        }
+    }
+}
---
+++ 7.25/favorite_stars/favorite_stars/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace favorite_stars
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new favorite_star());
+        }
+    }
+}
