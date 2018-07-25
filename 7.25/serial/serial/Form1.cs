6b666cb59ce74f15acfe0e27706c57cad73cde8f
Menboong <ruby020415@naver.com>
2018-07-25 23:30:21 +0900

    Serial_with_arduino
    I made this program at jumpup_camp.
    You can communication with arduino. when you push button, you can connect with that. and when you push on button, it send 1, and when push off button, it send 0.

---
+++ 7.25/serial/serial/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace serial
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (serialPort1.IsOpen == false) serialPort1.Open();
+            if (serialPort1.IsOpen == true) button1.BackColor = Color.GreenYellow;
+            else button1.BackColor = Color.Red;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            serialPort1.Write("1");
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            serialPort1.Write("0");
+        }
+    }
+}
