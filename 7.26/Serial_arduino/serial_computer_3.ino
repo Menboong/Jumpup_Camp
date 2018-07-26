be4e5465a9760a5332ae5921426c71a5ce367543
Menboong <ruby020415@naver.com>
2018-07-26 22:10:59 +0900

    Serial_with_computer

---
+++ 7.26/Serial_arduino/serial_computer_3.ino
@@ -0,0 +1,36 @@
+int TX=0;
+int RX[2] = {0,0};
+int i;
+int sum;
+
+void setup() {
+Serial.begin(9600);
+pinMode(13,OUTPUT);
+pinMode(4,OUTPUT);
+}
+
+void loop() {
+serialEvent();
+/*if(RX == 5149)digitalWrite(13,1);
+else if(RX == 5048) digitalWrite(13,0);
+else if(RX == "23") digitalWrite(4,1);
+else if(RX == "39") digitalWrite(4,0);*/
+if(i==2)
+{
+  sum = RX[0] + RX[1];
+  i=0;
+  Serial.print(RX[0]);
+  Serial.println(RX[1]);
+
+  
+}
+}
+
+void serialEvent(){
+  if(Serial.available()>0)
+  {
+    RX[i] = Serial.read();
+    i++;
+  }
+}
+
