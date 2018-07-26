5503d59f955d46dd8f342687d1d858bff03de299
Menboong <ruby020415@naver.com>
2018-07-26 11:20:48 +0900

    Serial_with_computer
    I made this at jumpup_camp.
    I add progress bar that see value of rxD.

--- 7.26/serial/serial/Form1.Designer.cs
+++ 7.26/serial/serial/Form1.Designer.cs
@@ -40,6 +40,10 @@
             this.groupBox1 = new System.Windows.Forms.GroupBox();
             this.groupBox2 = new System.Windows.Forms.GroupBox();
             this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
             this.groupBox1.SuspendLayout();
             this.groupBox2.SuspendLayout();
             this.SuspendLayout();
@@ -47,6 +51,11 @@
             // serialPort1
             // 
             this.serialPort1.PortName = "COM23";
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 700;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
             // 
             // button1
             // 
@@ -124,7 +133,6 @@
             this.groupBox1.TabIndex = 6;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Buzzer";
-            this.groupBox1.Enter += new System.EventHandler(this.groupBox1_Enter);
             // 
             // groupBox2
             // 
@@ -143,24 +151,66 @@
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("NanumBarunGothic", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
-            this.label2.Location = new System.Drawing.Point(201, 69);
+            this.label2.Location = new System.Drawing.Point(206, 75);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(43, 15);
             this.label2.TabIndex = 8;
-            this.label2.Text = "label2";
+            this.label2.Text = "NONE";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("NanumBarunGothic", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.label3.Location = new System.Drawing.Point(82, 324);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(43, 15);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "label3";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("NanumBarunGothic", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.label4.Location = new System.Drawing.Point(156, 75);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(47, 15);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "통신값:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("NanumBarunGothic", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.label5.Location = new System.Drawing.Point(32, 324);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(47, 15);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "센서값:";
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(36, 343);
+            this.progressBar1.Maximum = 1023;
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(214, 11);
+            this.progressBar1.TabIndex = 12;
+            this.progressBar1.Click += new System.EventHandler(this.progressBar1_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(294, 379);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.groupBox2);
             this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "Form1";
-            this.Load += new System.EventHandler(this.Form1_Load);
             this.groupBox1.ResumeLayout(false);
             this.groupBox2.ResumeLayout(false);
             this.groupBox2.PerformLayout();
@@ -182,6 +232,10 @@
         private System.Windows.Forms.GroupBox groupBox1;
         private System.Windows.Forms.GroupBox groupBox2;
         private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.ProgressBar progressBar1;
     }
 }
 
--- 7.26/serial/serial/Form1.cs
+++ 7.26/serial/serial/Form1.cs
@@ -11,6 +11,8 @@ namespace serial
 {
     public partial class Form1 : Form
     {
+        string rxD;
+        int time=0;
         public Form1()
         {
             InitializeComponent();
@@ -19,28 +21,51 @@ namespace serial
         private void button1_Click(object sender, EventArgs e)
         {
             if (serialPort1.IsOpen == false) serialPort1.Open();
-            if (serialPort1.IsOpen == true) button1.BackColor = Color.GreenYellow;
+            if (serialPort1.IsOpen == true)
+            {
+                button1.BackColor = Color.GreenYellow;
+                timer1.Enabled = true;
+            }
             else button1.BackColor = Color.Red;
         }
 
         private void button2_Click(object sender, EventArgs e)
         {
-            serialPort1.Write("1");
+            serialPort1.Write("31");
         }
 
         private void button3_Click(object sender, EventArgs e)
         {
-            serialPort1.Write("0");
+            serialPort1.Write("20");
         }
 
         private void button5_Click(object sender, EventArgs e)
         {
-            serialPort1.Write("3");
+            serialPort1.Write("39");
         }
 
         private void button4_Click(object sender, EventArgs e)
         {
-            serialPort1.Write("2");
+            serialPort1.Write("23");
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            rxD = serialPort1.ReadExisting();
+            time++;
+            if (rxD != "")
+            {
+                if (time > 1)
+                {
+                    label3.Text = rxD;
+                    progressBar1.Value = Convert.ToInt32(rxD);
+                } 
+            }
+        }
+
+        private void progressBar1_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
