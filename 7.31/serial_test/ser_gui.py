d7f9facbe5f4e6af6d35d66e80828cfda4dc75fc
Menboong <ruby020415@naver.com>
2018-08-01 23:11:20 +0900

    Serial_test_gui
    This is code of Serial_test_gui.

---
+++ 7.31/serial_test/ser_gui.py
@@ -0,0 +1,52 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'ser_gui.ui'
+#
+# Created by: PyQt5 UI code generator 5.11.2
+#
+# WARNING! All changes made in this file will be lost!
+
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName("MainWindow")
+        MainWindow.resize(481, 239)
+        self.centralwidget = QtWidgets.QWidget(MainWindow)
+        self.centralwidget.setObjectName("centralwidget")
+        self.sendButton = QtWidgets.QPushButton(self.centralwidget)
+        self.sendButton.setGeometry(QtCore.QRect(350, 160, 81, 31))
+        self.sendButton.setObjectName("sendButton")
+        self.sendEdit = QtWidgets.QLineEdit(self.centralwidget)
+        self.sendEdit.setGeometry(QtCore.QRect(50, 160, 271, 31))
+        self.sendEdit.setObjectName("sendEdit")
+        self.rxBrowser = QtWidgets.QTextBrowser(self.centralwidget)
+        self.rxBrowser.setGeometry(QtCore.QRect(50, 10, 381, 141))
+        self.rxBrowser.setObjectName("rxBrowser")
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.menubar = QtWidgets.QMenuBar(MainWindow)
+        self.menubar.setGeometry(QtCore.QRect(0, 0, 481, 17))
+        self.menubar.setObjectName("menubar")
+        MainWindow.setMenuBar(self.menubar)
+        self.statusbar = QtWidgets.QStatusBar(MainWindow)
+        self.statusbar.setObjectName("statusbar")
+        MainWindow.setStatusBar(self.statusbar)
+
+        self.retranslateUi(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+    def retranslateUi(self, MainWindow):
+        _translate = QtCore.QCoreApplication.translate
+        MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
+        self.sendButton.setText(_translate("MainWindow", "SEND"))
+
+
+if __name__ == "__main__":
+    import sys
+    app = QtWidgets.QApplication(sys.argv)
+    MainWindow = QtWidgets.QMainWindow()
+    ui = Ui_MainWindow()
+    ui.setupUi(MainWindow)
+    MainWindow.show()
+    sys.exit(app.exec_())
+
